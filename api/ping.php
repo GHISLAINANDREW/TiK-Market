c7333e12e1fca8e8dcfd0c2e0e115663c82912aa
DschangMarket <dschangmarket@email.com>
2026-08-11 23:02:33 +0200

    Deployment test 2: update version to 1.0.2

--- api/ping.php
+++ api/ping.php
@@ -1,3 +1,5 @@
 <?php
+<?php
+// DEPLOYMENT TEST 12345
 header('Content-Type: application/json');
-echo json_encode(['status' => 'ok', 'server' => 'render', 'version' => '1.0.1', 'time' => time()]);
+echo json_encode(['status' => 'ok', 'server' => 'render', 'version' => '1.0.2', 'time' => time()]);
