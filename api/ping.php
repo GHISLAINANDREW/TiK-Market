50fbbdbec32dae8127e21ffba9c381ff9b166162
DschangMarket <dschangmarket@email.com>
2026-08-11 22:44:39 +0200

    Add check.php and update ping.php for deployment verification

--- api/ping.php
+++ api/ping.php
@@ -1,3 +1,3 @@
 <?php
 header('Content-Type: application/json');
-echo json_encode(['status' => 'ok', 'server' => 'render', 'time' => time()]);
+echo json_encode(['status' => 'ok', 'server' => 'render', 'version' => '1.0.1', 'time' => time()]);
