5156ed522ceefc0845852f19057b709386bf462a
DschangMarket <dschangmarket@email.com>
2026-08-11 23:45:44 +0200

    Cleanup deployment test

--- api/check.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo "Files in current directory:\n";
-$files = scandir(__DIR__);
-foreach ($files as $file) {
-    echo $file . "\n";
-}
-echo "\nDocument Root: " . $_SERVER['DOCUMENT_ROOT'] . "\n";
-echo "Script Filename: " . $_SERVER['SCRIPT_FILENAME'] . "\n";
--- api/ping.php
+++ api/ping.php
@@ -1,5 +1,3 @@
 <?php
-<?php
-// DEPLOYMENT TEST 12345
 header('Content-Type: application/json');
-echo json_encode(['status' => 'ok', 'server' => 'render', 'version' => '1.0.2', 'time' => time()]);
+echo json_encode(['status' => 'ok', 'server' => 'render', 'time' => time()]);
